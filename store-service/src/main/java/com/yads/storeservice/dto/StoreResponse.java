ed266f9f0d04ba170c5f51cfb4f87b7a391f241c
devwelkin <112391727+devwelkin@users.noreply.github.com>
2025-10-08 11:39:02 +0300

    feat: Add CreateStoreRequest and StoreResponse DTOs for store management

--- store-service/src/main/java/com/yads/storeservice/dto/StoreResponse.java
+++ store-service/src/main/java/com/yads/storeservice/dto/StoreResponse.java
@@ -4,10 +4,14 @@ import com.yads.storeservice.model.Address;
 import com.yads.storeservice.model.StoreType;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
-public class CreateStoreRequest {
+public class StoreResponse {
+    private UUID id;
     private String name;
     private String description;
     private StoreType storeType;
+    private Boolean isActive;
     private Address address;
 }
