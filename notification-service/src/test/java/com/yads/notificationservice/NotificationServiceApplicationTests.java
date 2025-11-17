21fe612deb774d6e26fde923261b57537407ebb7
devwelkin <112391727+devwelkin@users.noreply.github.com>
2025-11-17 11:56:41 +0300

    initalize notification service

---
+++ notification-service/src/test/java/com/yads/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yads.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
